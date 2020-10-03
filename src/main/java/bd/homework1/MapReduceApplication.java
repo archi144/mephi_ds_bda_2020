0c25ff53953ec9101c58e5db4d5bf78b1fa8e7d6
archi144 <mechetin.artur2013@gmail.com>
2020-10-03 13:12:57 -0400

    refactoring

--- src/main/java/bd/homework1/MapReduceApplication.java
+++ src/main/java/bd/homework1/MapReduceApplication.java
@@ -3,13 +3,17 @@ package bd.homework1;
 import lombok.extern.log4j.Log4j;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 
 
 @Log4j
@@ -21,23 +25,28 @@ public class MapReduceApplication {
             throw new RuntimeException("You should specify input and output folders!");
         }
         Configuration conf = new Configuration();
-        // задаём выходной файл, разделенный запятыми - формат CSV в соответствии awdwadwf
-        conf.set("mapreduce.output.textoutputformat.separator", ":");
 
-        Job job = Job.getInstance(conf, "browser count");
+        Job job = Job.getInstance(conf, "longest word[s] searcher");
         job.setJarByClass(MapReduceApplication.class);
         job.setMapperClass(HW1Mapper.class);
         job.setReducerClass(HW1Reducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
         Path outputDirectory = new Path(args[1]);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, outputDirectory);
         log.info("=====================JOB STARTED=====================");
         job.waitForCompletion(true);
         log.info("=====================JOB ENDED=====================");
-        // проверяем статистику по счётчикам
+        SequenceFile.Reader reader = new SequenceFile.Reader(FileSystem.get(conf), new Path(args[1]+"/part-r-00000"), conf);
+        WritableComparable key = (WritableComparable) reader.getKeyClass().newInstance();
+        Writable value = (Writable) reader.getValueClass().newInstance();
+        log.info("=====================RESULT========================");
+        while (reader.next(key, value))
+            System.out.println(key + " " + value);
+        reader.close();
+        // print counters
         Counter counter = job.getCounters().findCounter(CounterType.MALFORMED);
         log.info("=====================COUNTERS " + counter.getName() + ": " + counter.getValue() + "=====================");
     }
